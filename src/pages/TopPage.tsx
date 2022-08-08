d4bd943946797ad4af599bb0b4221f252bb460ae
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-08-08 10:55:44 +0900

    ルーティング設定完了

---
+++ src/pages/TopPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+export default function TopPage() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
