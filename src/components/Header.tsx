850e6f0073e231029d97fcac055dcf250e245cb4
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-08-08 11:52:42 +0900

    cssの調整完了

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div id="header">
+      <div className="container">
+        <h1>COVID-19 Japan</h1>
+        <h2>新型コロナウイルス対策ダッシュボード</h2>
+      </div>
+    </div>
+  );
 }
