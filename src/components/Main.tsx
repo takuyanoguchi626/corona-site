06b4ce740eb74da1a6725e264a38f350edd4ad68
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-08-09 10:48:23 +0900

    右側の表のレイアウトが完成

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,42 +1,59 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-export default function main() {
+export default function Main() {
+  const [data, setData] = useState([]);
+
+  const axiosGet = async () => {
+    const res = await axios.get(
+      "https://www.stopcovid19.jp/data/covid19japan_beds/latest.json"
+    );
+    setData(() => res.data);
+  };
+
+  useEffect(() => {
+    axiosGet();
+  }, []);
+  console.log(data);
+
   return (
     <div id="main">
       <div className="upper">
         <div className="left">
           <div className="patientChart">
             <table border={1}>
-              <tr>
-                <td>現在患者数/対策病床数</td>
-                <td>現在患者数</td>
-              </tr>
-              <tr>
-                <td>a</td>
-                <td>a</td>
-              </tr>
-              <tr>
-                <td>累積退院者</td>
-                <td>死亡者</td>
-              </tr>
-              <tr>
-                <td>a</td>
-                <td>a</td>
-              </tr>
-              <tr>
-                <td>対策病床数 115,400床</td>
-                <td>PCR検査陽性者数 14,055,302人</td>
-              </tr>
-              <tr>
-                <td colSpan={2}>
-                  臨床工学技士 14,378人 / 人工呼吸器 28,197台 / ECMO 1,412台
-                  2020年2月回答 出典
-                  <a href="https://ja-ces.or.jp/info-ce/%e4%ba%ba%e5%b7%a5%e5%91%bc%e5%90%b8%e5%99%a8%e3%81%8a%e3%82%88%e3%81%b3ecmo%e8%a3%85%e7%bd%ae%e3%81%ae%e5%8f%96%e6%89%b1%e5%8f%b0%e6%95%b0%e7%ad%89%e3%81%ab%e9%96%a2%e3%81%99%e3%82%8b%e7%b7%8a/">
-                    一般社団法人 日本呼吸療法医学会　公益社団法人
-                    日本臨床工学技士会
-                  </a>
-                </td>
-              </tr>
+              <tbody>
+                <tr>
+                  <td>現在患者数/対策病床数</td>
+                  <td>現在患者数</td>
+                </tr>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+                <tr>
+                  <td>累積退院者</td>
+                  <td>死亡者</td>
+                </tr>
+                <tr>
+                  <td>a</td>
+                  <td>a</td>
+                </tr>
+                <tr>
+                  <td>対策病床数 115,400床</td>
+                  <td>PCR検査陽性者数 14,055,302人</td>
+                </tr>
+                <tr>
+                  <td colSpan={2}>
+                    臨床工学技士 14,378人 / 人工呼吸器 28,197台 / ECMO 1,412台
+                    2020年2月回答 出典
+                    <a href="https://ja-ces.or.jp/info-ce/%e4%ba%ba%e5%b7%a5%e5%91%bc%e5%90%b8%e5%99%a8%e3%81%8a%e3%82%88%e3%81%b3ecmo%e8%a3%85%e7%bd%ae%e3%81%ae%e5%8f%96%e6%89%b1%e5%8f%b0%e6%95%b0%e7%ad%89%e3%81%ab%e9%96%a2%e3%81%99%e3%82%8b%e7%b7%8a/">
+                      一般社団法人 日本呼吸療法医学会　公益社団法人
+                      日本臨床工学技士会
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
             </table>
           </div>
           <div className="patientSentence">
@@ -86,7 +103,18 @@ export default function main() {
             </div>
           </div>
         </div>
-        <div className="right">都道府県別の表</div>
+        <div className="right">
+          <div className="prefectures">
+            <div className="wholeCountry">aaa</div>
+            {data.map((a, index) => {
+              return (
+                <div key={index} className="prefecture">
+                  sss
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <div className="lower">
         <p>
